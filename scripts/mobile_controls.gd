90b76c3f21b1a32e5c62e840077e24de241d01ab
Jesus Surga <106843028+Surgaj@users.noreply.github.com>
2026-09-26 10:58:29 -0300

    fix: fit expanded character sheet on mobile landscape

--- scripts/mobile_controls.gd
+++ scripts/mobile_controls.gd
@@ -128,9 +128,11 @@ func layout():
 	portrait_warning.position = size*0.5-Vector2(190,47)
 	portrait_warning.visible = size.y>size.x
 	inventory_button.position = Vector2(size.x-170,140)
-	var ui_scale = clampf(size.x/1150.0,1.0,1.35)
+	var width_scale = clampf(size.x/1150.0,0.78,1.35)
+	var height_scale = clampf(size.y/520.0,0.75,1.35)
+	var ui_scale = minf(width_scale,height_scale)
 	inventory_panel.scale = Vector2.ONE*ui_scale
-	inventory_panel.size = Vector2(minf(930,(size.x-64)/ui_scale),minf(500,(size.y-64)/ui_scale))
+	inventory_panel.size = Vector2(minf(930,(size.x-48)/ui_scale),minf(500,(size.y-40)/ui_scale))
 	inventory_panel.position = (size-inventory_panel.size*ui_scale)*0.5
 	joystick.queue_redraw()
 func open_inventory(shop_id := ""):
